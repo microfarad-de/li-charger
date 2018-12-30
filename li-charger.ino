7fca471ca7c107f910626d685ed8d4b9ef1dabea
Karim <khraibi@gmail.com>
2018-12-30 21:37:18 +0100

    Tested with 2s LiIon

--- li-charger.ino
+++ li-charger.ino
@@ -16,19 +16,28 @@
 
 
 
-#define NUM_APINS 2            // Number of analog pins in use
+#define NUM_APINS    2         // Number of analog pins in use
 #define VOLTAGE_APIN ADC_PIN0  // Analog pin for voltage measurement
 #define CURRENT_APIN ADC_PIN1  // Analog pin for current measurement
 
-#define MOSFET_PIN 9           // PWM pin controlling the gate of the power MOSFET
-#define LED_PIN 13             // LED pin
+#define MOSFET_PIN      9      // PWM pin controlling the gate of the power MOSFET
+#define LED_PIN        13      // LED pin
+#define ADC_DIVIDE_PIN  8      // Pin for connecting the ADC voltage divider to the negative rail
+                               // ADC voltage divider must be disconnected to avoid battery drain
 
-#define VOLT_REFERENCE 1000000 // 1.00 Volt - Reference voltage used for calibration in uV
-#define VOLT_MAX       4180000 // 4.15 Volt - Maximum allowable battery voltage in uV
-#define VOLT_MIN       2600000 // 2.60 Volt - Minimum allowable battery voltage in uV
-#define VOLT_WINDOW      10000 // 0.01 Volt - Do not regulate when within +/- this window in uV
-#define UPDATE_INTERVAL     50 // Interval for updating the power output in ms 
-#define IIR_FILTER_TAPS      4 // IIR fiter taps for smoothing the ADC output
+#define VOLT_CALIBRATION_V1 8400000 // 8.40 V - Calibrate V1 against this voltage in uV
+#define VOLT_CALIBRATION_V2 1000000 // 1.00 V - Calibrate V2 against thsi voltage in uV
+#define VOLT_MAX            8380000 // 8.38 V - Maximum allowed battery voltage in uV
+#define VOLT_MIN            5200000 // 5.20 V - Minimum allowed battery voltage in uV
+#define VOLT_WINDOW           20000 // 0.02 V - Do not regulate when within +/- this window in uV
+#define CURR_CALIBRATION_P      100 // Determines number of digits of the current calibration value (precision)
+#define CURR_MAX             500000 // 0.50 A - Maximum allowed current in uA
+#define CURR_WINDOW           10000 // 0.01 A - Do not regulate when within +/- this window in uA
+#define CURR_FULL             50000 // 0.05 A - The battery is considered full if this current in uA is not exceeded during TIMEOUT_FULL
+#define TIMEOUT_FULL          60000 // Time duration in ms during which CURR_FULL shall not be exceeded in order to assume that battery is full
+#define UPDATE_INTERVAL          50 // Interval for updating the power output in ms 
+#define IIR_FILTER_TAPS           2 // IIR fiter taps for smoothing the ADC output
+
 
 // Code snippets
 #define EEPROM_READ() eepromRead (0x0, (uint8_t*)&Nvm, sizeof (Nvm));
@@ -42,12 +51,13 @@ LedClass Led;    // LED object
  */
 struct {
   uint8_t analogPin[NUM_APINS] = { VOLTAGE_APIN, CURRENT_APIN };  // Analog pins as an array
-  int16_t v1Raw;  // Raw ADC value of v1
-  int16_t v2Raw;  // Raw ADC value of v2
-  int32_t v1;     // Voltage at the battery '+' terminal (MOSFET drain) in uV
-  int32_t v2;     // Voltage at the battery '-' terminal (shunt) in uV
-  int32_t i;      // Current
-  int16_t dutyCycle;
+  int16_t v1Raw;  // Raw ADC value of V1
+  int16_t v2Raw;  // Raw ADC value of V2
+  int32_t v1;     // V1 - Voltage at the battery '+' terminal (MOSFET drain) in uV
+  int32_t v2;     // V2 - Voltage at the battery '-' terminal (shunt) in uV
+  int32_t vBatt;  // Vbatt - Battery voltage = V1 - V2
+  int32_t i;      // I - Current
+  int16_t dutyCycle; // PWM duty cycle
 } G;
 
 
@@ -73,10 +83,9 @@ void setup (void) {
   Cli.xputs ("");
   Cli.xprintf ("Version: %d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_MAINT);
   Cli.xputs ("");
-  Cli.newCmd ("p", "set PWM: 0..255", cmdPwm);
   Cli.newCmd ("v1cal", "calibrate V1", v1Calibrate);
   Cli.newCmd ("v2cal", "calibrate V2", v2Calibrate);
-  Cli.newCmd ("ical", "calibrate I", iCalibrate);
+  Cli.newCmd ("ical", "calibrate I (shunt R in mΩ as arg)", iCalibrate);
   Cli.newCmd ("c", "show calibration", showCalibration);
   Cli.newCmd (".", "show results", cmdResult);
   
@@ -85,7 +94,9 @@ void setup (void) {
   // Initialize the ADC
   ADConv.initialize (ADC_PRESCALER_128, ADC_INTERNAL, NUM_APINS);
 
+  // Initialize digital pins
   pinMode (MOSFET_PIN, OUTPUT);
+  pinMode (ADC_DIVIDE_PIN, OUTPUT);
 
   Led.initialize (LED_PIN);
 
@@ -98,9 +109,10 @@ void setup (void) {
  * Arduino main loop
  */
 void loop (void) {
-  static enum { STATE_INIT_E, STATE_INIT, STATE_CHARGE_E, STATE_CHARGE, STATE_FULL_E, STATE_FULL, STATE_ERROR_E, STATE_ERROR } state = STATE_INIT_E;
+  static enum { STATE_INIT_E, STATE_INIT, STATE_CHARGE_E, STATE_CHARGE, 
+      STATE_FULL_E, STATE_FULL, STATE_ERROR_E, STATE_ERROR } state = STATE_INIT_E;
   static uint32_t updateTs = 0;
-  int32_t vBatt;
+  static uint32_t fullTs = 0;
   uint32_t ts = millis ();
   
   // Command-line interpreter
@@ -112,6 +124,7 @@ void loop (void) {
   // Read the ADC channels
   adcRead ();
 
+
   // Main state machine
   switch (state) {
 
@@ -119,12 +132,17 @@ void loop (void) {
     /**********************************/
     // Initializing
     case STATE_INIT_E:
-      Led.blink (-1, 200, 800);
+      Led.blink (-1, 400, 1600);
+      digitalWrite (ADC_DIVIDE_PIN, HIGH);
       G.dutyCycle = 0;
+      Cli.xputs ("Waiting for battery\n");
       state = STATE_INIT; 
       
     case STATE_INIT:
-      state = STATE_CHARGE_E;
+      // Check if a battery is connected
+      if ( G.vBatt > (int32_t)VOLT_MIN && G.vBatt < (int32_t)VOLT_MAX ) {
+        state = STATE_CHARGE_E;
+      }
       break;
 
 
@@ -132,26 +150,35 @@ void loop (void) {
     // Charging
     case STATE_CHARGE_E:
       Led.turnOn ();
+      fullTs = ts;
+      Cli.xputs ("Charging\n");
       state = STATE_CHARGE;
       
     case STATE_CHARGE:
       if (ts - updateTs > UPDATE_INTERVAL) {
         updateTs = ts;
-        vBatt = G.v1 - G.v2;
 
         // Abort if voltage out of bounds
-        //if (vBatt > ((int32_t)VOLT_MAX + 10*(int32_t)VOLT_WINDOW)) state = STATE_ERROR_E;
-        //if (vBatt < VOLT_MIN)                 state = STATE_ERROR_E;
+        if (G.vBatt > ((int32_t)VOLT_MAX + 10*(int32_t)VOLT_WINDOW)) state = STATE_ERROR_E;
+        if (G.vBatt < (int32_t)VOLT_MIN)                             state = STATE_ERROR_E;
 
-        // Regulate voltage
-        if (vBatt > (int32_t)VOLT_MAX + (int32_t)VOLT_WINDOW) {
+        // Regulate voltage and current
+        if ( ( G.vBatt > (int32_t)VOLT_MAX + (int32_t)VOLT_WINDOW ) ||
+             ( G.i     > (int32_t)CURR_MAX + (int32_t)CURR_WINDOW ) ) {
           if (G.dutyCycle > 0) G.dutyCycle--;
         }
-        else if (vBatt < (int32_t)VOLT_MAX - (int32_t)VOLT_WINDOW) {
+        else if ( ( G.vBatt < (int32_t)VOLT_MAX - (int32_t)VOLT_WINDOW ) &&
+                  ( G.i     < (int32_t)CURR_MAX - (int32_t)CURR_WINDOW ) ) {
           if (G.dutyCycle < 255) G.dutyCycle++;
         }
 
-        analogWrite (MOSFET_PIN, (int8_t)G.dutyCycle);  
+        analogWrite (MOSFET_PIN, (int8_t)G.dutyCycle);
+
+        // Reset battery full timer
+        if (G.i > (int32_t)CURR_FULL) fullTs = ts;
+
+        // Battery full
+        if (ts - fullTs > TIMEOUT_FULL) state = STATE_FULL_E;
       }
       
       break;
@@ -160,7 +187,10 @@ void loop (void) {
     /**********************************/
     // Battery full
     case STATE_FULL_E:
-      Led.blink (-1, 200, 800);
+      Led.blink (-1, 100, 1900);
+      analogWrite (MOSFET_PIN, 0);
+      digitalWrite (ADC_DIVIDE_PIN, LOW);
+      Cli.xputs ("Battery full\n");
       state = STATE_FULL;
       
     case STATE_FULL:
@@ -173,6 +203,8 @@ void loop (void) {
     case STATE_ERROR_E:
       Led.blink (-1, 200, 200);
       analogWrite (MOSFET_PIN, 0);
+      digitalWrite (ADC_DIVIDE_PIN, LOW);
+      Cli.xputs ("ERROR\n");
       state = STATE_ERROR;
       
     case STATE_ERROR:
@@ -208,6 +240,8 @@ void adcRead (void) {
 
     G.v1 = (int32_t)G.v1Raw * Nvm.v1Calibration;
     G.v2 = (int32_t)G.v2Raw * Nvm.v2Calibration;
+    G.vBatt = G.v1 - G.v2;
+    G.i  = ( (int32_t)G.v2 * Nvm.iCalibration ) / CURR_CALIBRATION_P ;
   }
 }
 
@@ -225,6 +259,7 @@ int cmdResult (int argc, char **argv) {
   Cli.xprintf ("V2 (raw) = %d\n", G.v2Raw);
   Cli.xprintf ("V1       = %ld\n", G.v1);
   Cli.xprintf ("V2       = %ld\n", G.v2);
+  Cli.xprintf ("Vbatt    = %ld\n", G.vBatt);
   Cli.xprintf ("I        = %ld\n", G.i);
   Cli.xputs("");
   return 0;
@@ -247,7 +282,7 @@ int showCalibration (int argc, char **argv) {
  * Calibrate V1
  */
 int v1Calibrate (int argc, char **argv) {
-  Nvm.v1Calibration = (int32_t)(5*VOLT_REFERENCE/G.v1Raw);
+  Nvm.v1Calibration = (int32_t)VOLT_CALIBRATION_V1 / (int32_t)G.v1Raw;
   EEPROM_WRITE()
   Cli.xprintf("V1 cal. = %ld\n", Nvm.v1Calibration);
   Cli.xputs("");
@@ -259,7 +294,7 @@ int v1Calibrate (int argc, char **argv) {
  * Calibrate V2
  */
 int v2Calibrate (int argc, char **argv) {
-  Nvm.v2Calibration = (int32_t)(VOLT_REFERENCE/G.v2Raw);
+  Nvm.v2Calibration = (int32_t)VOLT_CALIBRATION_V2 / (int32_t)G.v2Raw;
   EEPROM_WRITE()
   Cli.xprintf("V2 cal. = %ld\n", Nvm.v2Calibration);
   Cli.xputs("");
@@ -269,22 +304,13 @@ int v2Calibrate (int argc, char **argv) {
 
 /*
  * Calibrate I
+ * argv{1} = shunt resistance in milliohm
  */
 int iCalibrate (int argc, char **argv) {
+  int32_t r = atoi (argv[1]);
+  Nvm.iCalibration = ((int32_t)CURR_CALIBRATION_P * 1000) / r;
   EEPROM_WRITE()
-  Cli.xputs("");
-  return 0;
-}
-
-
-
-/*
- * Set the duty cycle of the PWM output
- */
-int cmdPwm (int argc, char **argv) {
-  uint8_t val = atoi (argv[1]);
-  Cli.xprintf ("%d\n", val);
-  analogWrite (MOSFET_PIN, val);
+  Cli.xprintf("I cal. = %ld\n", Nvm.iCalibration);
   Cli.xputs("");
   return 0;
 }
