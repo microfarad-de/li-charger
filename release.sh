2ba55c9eddc120300ba5f27abe076bea493df9dc
Karim Hraibi <khraibi@apple.com>
2022-04-11 10:05:17 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ fi
 rm -r "$file_name-"*"-full"*
 
 if [[ $version != "clean" ]]; then
-  zip -r "$file_name-$version-full.zip" . -x '*.git*' '*.vscode*' '*private*' '*.DS_Store'
+  zip -r "$file_name-$version-full.zip" . -x '*.git*' '*.vscode*' '*private*' '*build*' '*.DS_Store'
 fi
 
 exit 0
