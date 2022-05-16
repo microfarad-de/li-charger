b0ebe48b17eb7683d054933c4fe973c8bf09c69b
Karim Hraibi <khraibi@apple.com>
2022-05-16 08:57:25 +0200

    Updated release.sh

--- release.sh
+++ release.sh
@@ -2,19 +2,28 @@
 # Create a zip file containing the source code including submodules
 # Usage: release <version>
 
-version="$1"
+option="$1"
 file_name="li-charger"
+
+version_major=$(cat "$file_name.ino" | grep '#define VERSION_MAJOR' | awk -F' ' '{print $3}')
+version_minor=$(cat "$file_name.ino" | grep '#define VERSION_MINOR' | awk -F' ' '{print $3}')
+version_maint=$(cat "$file_name.ino" | grep '#define VERSION_MAINT' | awk -F' ' '{print $3}')
+
+version="$version_major.$version_minor.$version_maint"
 
 if [[ -z $version ]]; then
   echo "Error: No version string specified"
-  echo "Usage: $0 <version>"
   exit 1
 fi
 
+sed -i -e "s/.* * Version:.*/ * Version: $version/" "$file_name.ino"
+sed -i -e "s/.* * Date:.*/ * Date:    $(date '+%B %d, %Y')/" "$file_name.ino"
+rm -rf "$file_name.ino-e"
+
 rm -rf "$file_name-"*"-full"*
 
-if [[ $version != "clean" ]]; then
-  zip -r "$file_name-$version-full.zip" . -x '*.git*' '*.vscode*' '*private*' '*build*' '*.DS_Store'
+if [[ "$option" != "clean" ]]; then
+  zip -r "$file_name-$version-full.zip" . -x '*.git*' '*.vscode*' '*private*' '*build-*' '*.DS_Store*'
 fi
 
 exit 0
